d74e40c447f1c39603b3238813d8756680b51353
pmtofficial <89723530+pmtofficial@users.noreply.github.com>
2021-10-18 22:46:23 +0700

    Update giaibai.pas

--- giaibai.pas
+++ giaibai.pas
@@ -1,7 +1,41 @@
-program baitap;
+program bai1;
 users crt;
 begin 
   clrscr;
-  writeln('Chuong Trinh PasCal');
+  writeln('(15 * 4) - (30 + 12) =', (15 * 4) - (30 + 12));
+  writeln('(10 +5) / (3 + 1) - (18 / (5 + 1)) =', ((10 +5) / (3 + 1)) - (18 / (5 + 1));
+  writeln('(10 + 2) * (10 + 2) / (3 + 1) =', ((10 + 2) * (10 + 2)) / (3 + 1));
+  writeln('((10 + 2) * (10 + 2) - 24)) / ( 3 + 1) =', ((10 + 2) * (10 + 2) - 24)) / ( 3 + 1));
+  writeln('Nguyễn Phương Anh')
   readln;
 end.
+
+
+program bai2;
+users crt;
+begin
+  clrscr;
+  writeln('16/3 =', 16/3);
+  delay(5000);
+  writeln('16 div 3 =', 16 div 3);
+  delay(5000);
+  writeln('16 mod 3 =', 16 mov 3);
+  delay(5000);
+  writeln('16 mod 3 =', 16-(16 div 3)*3);
+  delay(5000);
+  writeln('16 div 3 =', 16-(16 mod 3)/3);
+  writeln('Nguyễn Phương Anh')
+  readln;
+end.
+
+program bai3;
+users crt;
+begin
+  clrscr;
+  writeln('(15 * 4) - (30 + 12) =', (15 * 4) - (30 + 12));
+  writeln((10 +5) / (3 + 1) - 18 / (5 + 1):4:2);
+  writeln((10 + 2) * (10 + 2) / (3 + 1):4:2);
+  writeln(((10 + 2) * (10 + 2) - 24)) / ( 3 + 1):4:2;
+  writeln('Nguyễn Phương Anh')
+  readln;
+ end.
